8731d21f0ab19401fad9c294694134ac3bbda1b9
0zzysome <oscar.rosin@hotmail.com>
2022-11-15 13:31:22 +0100

    api

--- APIproject/Program.cs
+++ APIproject/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using RestSharp;
+using System.Text.Json;
+using System.Net;
+RestClient pokeClient = new("https://pokeapi.co/api/v2/");
+
+RestRequest request = new("pokemon/416");
+
+RestResponse response = pokeClient.GetAsync(request).Result;
+
+
+if (response.StatusCode == HttpStatusCode.OK)
+{
+    Pokemon p = JsonSerializer.Deserialize<Pokemon>(response.Content);
+
+    System.Console.WriteLine(p.Name);
+    System.Console.WriteLine(p.Weight);
+
+}
+else
+{
+    System.Console.WriteLine("not found");
+}
+
+Console.ReadLine();
